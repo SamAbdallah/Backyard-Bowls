dd27f8bd3eac90dfb32e68d3fd46de3e04bb3342
SamAbdallah <saa081@student.bau.edu.lb>
2023-06-02 12:51:45 +0300

    fixing styling for header

--- client-side/src/components/Header.jsx
+++ client-side/src/components/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import logo from "../rescources/logo.jpg" 
+// import logo from "../rescources/logo.jpg" 
+import logo2 from "../rescources/logo2.jfif" 
+import { FaShoppingCart } from 'react-icons/fa';
+
+
 import './Header.css'
 import { IoIosSearch } from 'react-icons/io';
 
@@ -9,7 +13,7 @@ import { IoIosSearch } from 'react-icons/io';
 function Header() {
   return (
     <div id='container'>
-    <img src={logo} alt='' id='image'/>
+    <img src={logo2} alt='' id='image'/>
     <p id='home'>Home</p>
     <p id='booking'>Booking</p>
     <div className='input'>
