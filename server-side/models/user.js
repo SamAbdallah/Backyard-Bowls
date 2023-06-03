a42c44f682c041b5b1124ed479932bbed808fc4a
SamAbdallah <saa081@student.bau.edu.lb>
2023-06-03 19:49:22 +0300

    adding user Model

--- server-side/models/user.js
+++ server-side/models/user.js
@@ -3,10 +3,47 @@ const Schema=mongoose.Schema
 
 const userSchema= new mongoose.Schema(
     {
-      
-        
 
+      fullName:{
+        type:String,
+        required:[true,"please enter full name"]
+      },
+      email:{
+        type:String,
+        required:[true,"please enter email"],
+        trim:true,
+        unique:true,
+        lowercase:true
+      },
+      password:{
+        type:String,
+        trim:true,
+        minLength:8
+      },
 
+      passwordConfirm:{
+        type:String,
+        trim:true,
+        minLength:8
+      },
 
-    }
+      type:{
+        type:string,
+        required:true
+      },
+      isVerified:{
+        type:Boolean,
+        default:false
+      },
+
+      passwordChangedAt:Date,
+      passwordResetToken:String,
+      passwordResetTokenExpires:Date,
+
+      validatedAt:Date,
+      validationToken:String,
+      validationExpires:Date
+
+    },
+    {timestamps:true}
 )
