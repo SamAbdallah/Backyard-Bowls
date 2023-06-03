4e90068e83a076ce92375db2b860097a919bd9f0
SamAbdallah <saa081@student.bau.edu.lb>
2023-06-03 19:37:51 +0300

    adding product model

---
+++ server-side/models/product.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+
+const productSchema= new mongoose.Schema(
+    {
+        productName:{
+            type:String,
+            required:[true,"Product name is required"],
+            trim:true,
+            maxlength:50,
+        },
+        productPrice:{
+
+            type:Number,
+            trim:true,
+            maxlength:[10,"too long price"]
+
+        },
+        category:{
+            type:String,
+            required:[true,"please enter category"]
+        },
+        description:{
+            type:String,
+            required:[true,"please enter description"]
+        }
+        
+
+
+
+    }
+)
+
+module.exports=mongoose.model("Product",productSchema)
