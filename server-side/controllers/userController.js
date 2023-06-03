84185b4ad0fffc1bb3d806cafe0c84ca120a938b
SamAbdallah <saa081@student.bau.edu.lb>
2023-06-03 20:09:41 +0300

    creating sign up function

---
+++ server-side/controllers/userController.js
@@ -0,0 +1,39 @@
+const User=require("../models/userModel")
+const validator=require("validator")
+
+exports.signUp=async(req,res)=>{
+    try{
+        let email=req.body.email 
+        if(!validator.isEmail(email)){
+            return res.status(400).json({message:"Invalid email"})
+        }
+
+        const checkEmail=await User.findOne({email:email})
+        if(checkEmail){
+            return res.status(409).json({message:"email already in use"})
+
+        }
+
+        let pass=req.body.password 
+        let passConfirm=req.body.passConfirm
+        if(pass!==passConfirm){
+            return res.status(400).json({message:"passwords do not match"})
+
+    }
+
+    
+    const newUser=await User.create({
+        fullName:req.body.fullName,
+        email:req.body.email,
+        password:req.body.password,
+        type:req.body.type
+                
+    })
+
+    return res.status(201).json({message:"user created successfully",data:newUser})
+}
+
+    catch(err){
+        console.log(err)
+    }
+}
