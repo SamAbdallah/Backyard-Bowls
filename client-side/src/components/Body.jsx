a090c45c4b94685d9a2761cfa839c6dbba820f1a
SamAbdallah <saa081@student.bau.edu.lb>
2023-06-09 23:09:54 +0300

    fixing body of home page

--- client-side/src/components/Body.jsx
+++ client-side/src/components/Body.jsx
@@ -3,7 +3,23 @@ import './Body.css'
 
 function Body() {
   return (
-    <h1>Body</h1>
+    <div className='container'>
+      <div>
+       <h1 id='title'>The Fastest <br/>Delivery  <br/> in <span id="orange">Your City</span></h1>
+       <h5 id='subtitle'>Get your fresh groceries or every morning <br/> delivered to your doorstep...Fastfood as well!</h5>
+       <div id='orderButton'><h1>Order Now</h1></div>
+      </div>
+
+      <div className='itemsTop'>
+        <div id='itemRow'>
+          <div>
+            
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
