20d0e2bf1e819f68eaa092eacbb6e5e1e63b899a
SamAbdallah <saa081@student.bau.edu.lb>
2023-06-10 03:55:38 +0300

    fixing stylings

--- client-side/src/components/Body.jsx
+++ client-side/src/components/Body.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Body.css'
-
+import burger from "../rescources/burger.jfif"
 function Body() {
   return (
     <div className='container'>
-      <div>
+      <div id='firstPart'>
        <h1 id='title'>The Fastest <br/>Delivery  <br/> in <span id="orange">Your City</span></h1>
        <h5 id='subtitle'>Get your fresh groceries or every morning <br/> delivered to your doorstep...Fastfood as well!</h5>
        <div id='orderButton'><h1>Order Now</h1></div>
@@ -12,8 +12,12 @@ function Body() {
 
       <div className='itemsTop'>
         <div id='itemRow'>
-          <div>
-            
+          <div id='item'>
+            <img src={burger} alt=''/>
+            <p>Burger</p>
+            <p>Classic meat burger</p>
+            <p>$10</p>
+            <p id='buttonsContainer'><span className='addRemove'>+</span>&nbsp;<span className='addRemove'>-</span></p>
           </div>
 
         </div>
